17b5f21e02c51dd1d493f9c4debd6fb5f1287bc4
Punit Juneja <punitjuneja123@gmail.com>
2023-05-27 15:11:34 +0530

    noCors

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middleware = jsonServer.defaults();
 
 server.use(jsonServer.bodyParser);
+server.use(jsonServer.noCors)
 server.use(middleware);
 server.use(router);
-server.use(cors());
+// server.use(cors());
 server.listen(4500, () => {
   console.log("json server running");
 });
